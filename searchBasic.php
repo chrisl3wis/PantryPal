c02d33e31ec0c4d01c95bf4447389487d12b10c8
Chris <imacman.g4@gmail.com>
2018-11-06 09:39:46 -0800

    Home page, Basic search and results

---
+++ searchBasic.php
@@ -0,0 +1,54 @@
+
+<?php
+
+echo "<hr>";
+$host = "webdev.iyaserver.com";
+$userid = "lewischr";
+$userpw = "Iya6521484446";
+$db = "lewischr_recipes";
+
+$mysqli = new mysqli (
+    $host,
+    $userid,
+    $userpw,
+    $db
+
+);
+if($mysqli -> connect_errno){
+    echo "db connection error" . $mysqli -> connect_error;
+    exit("STOPPING page");
+}
+
+
+?>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="generalStyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900|Montserrat" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>Pantry Pal</title>
+</head>
+<body>
+<div id="header">
+    <img src="pantrypal.png" id="masthead">
+    <a class="account" href=""> my account</a>
+</div>
+<div id="search_div">
+    <h1>What's in Your <br> Pantry Today?</h1>
+    <br>
+    <form action="resultsBasic.php">
+    <input type="text" class="ingredientFill" name="ingred1" placeholder="ingredient #1">
+    <br><br><br>
+    <input type="text" class="ingredientFill" name="ingred2" placeholder="ingredient #2">
+    <br><br><br>
+    <input type="text" class="ingredientFill" name="ingred3" placeholder="ingredient #3">
+    <br><br>
+
+    <a href="">advanced search >></a>
+    <br><br><br>
+
+    <input type="submit" class="search" value="search">
+    </form>
+</div>
+</body>
+</html>
