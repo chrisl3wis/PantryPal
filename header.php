be3377780a430c0b544e14420746e895972b9058
Chris <imacman.g4@gmail.com>
2018-11-07 23:01:45 -0800

    card based results

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div id="header">
-    <img src="pantrypal.png" id="masthead">
+    <a href="searchBasic.php"><img src="pantrypal.png" id="masthead"></a>
     <a class="account" href=""> my account</a>
 <!--Maybe if signed in have "Hello, Chris"-->
 </div>
--- resultsBasic.php
+++ resultsBasic.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $host = "webdev.iyaserver.com";
@@ -6,18 +5,14 @@ $userid = "lewischr";
 $userpw = "Iya6521484446";
 $db = "lewischr_recipes";
 
-$mysqli = new mysqli (
-    $host,
-    $userid,
-    $userpw,
-    $db
+$mysqli = new mysqli ($host, $userid, $userpw, $db);
 
-);
-if($mysqli -> connect_errno){
-    echo "db connection error" . $mysqli -> connect_error;
+if ($mysqli->connect_errno) {
+    echo "db connection error" . $mysqli->connect_error;
     exit("STOPPING page");
 }
 ?>
+
 <html lang="en">
 <head>
     <link rel="stylesheet" type="text/css" href="generalStyle.css">
@@ -25,7 +20,7 @@ if($mysqli -> connect_errno){
     <meta charset="UTF-8">
     <title>Pantry Pal</title>
     <style>
-        #resultsDiv{
+        #resultsDiv {
             width: 80%;
             background-color: white;
             height: auto;
@@ -37,87 +32,120 @@ if($mysqli -> connect_errno){
             border-radius: 20px;
 
         }
-        .resultsRecipe{
-            width: 20%;
-            float: left;
-            margin-right: 10px;
 
-        }
-        .resultsDescr{
-            width: 60%;
+        .searchResult {
+            width: 250px;
+            margin: 30px;
+            background-color: rgb(255, 255, 255);
+            border-radius: 10px;
+            padding-bottom: 15px;
+            -webkit-box-shadow: -1px 0px 7px 1px rgba(0, 0, 0, 0.1);
+            -moz-box-shadow: -1px 0px 7px 1px rgba(0, 0, 0, 0.1);
+            box-shadow: -1px 0px 7px 1px rgba(0, 0, 0, 0.1);
+            height: auto;
             float: left;
-            margin-right: 20px;
         }
-        .resultsHyper{
-            width: 10%;
-            float: left;
 
+        .recipeImage {
+            width: 100%;
+            margin-bottom: 0px;
+            position: relative;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
         }
-        /*.headerresults{*/
-            /*float: left;*/
-            /*font-weight: bold;*/
 
-        /*}*/
-        .HRecipe{
-            float: left;
-            font-weight: bold;
-            width: 20%;
-            margin-right: 10px;
-        }
-        .HDescr{
-            float: left;
-            font-weight: bold;
-            width: 60%;
-            margin-right: 20px;
-        }
-        .HLink{
-            float: left;
-            font-weight: bold;
-            width: 10%;
-        }
-        .resultslink{
+        body {
             font-family: 'Montserrat', sans-serif;
-            color: black;
+            font-size: 11pt;
+        }
+
+        .recipeInfo {
+            width: 230px;
+            margin: 10px;
+            color: grey;
+
+        }
+
+        .recipeName {
             font-size: 16pt;
+            line-height: 22pt;
+            font-weight: bold;
+            font-family: Lato, Helvetica, sans-serif;
+            color: black;
+        }
+
+        .tags {
+            padding: 2px 10px 2px 10px;
+            background-color: rgb(98, 168, 214);
+            border-radius: 10px;
+            margin-top: 10px;
+            margin-right: 5px;
+            float: left;
+            color: white;
+            font-size: 10pt;
+        }
+        .outLink {
+            height: 15px;
+            float: right;
         }
     </style>
 </head>
 <body>
-<div id="header">
-    <img src="pantrypal.png" id="masthead">
-    <a class="account" href=""> my account</a>
-</div>
+<?php
+include_once 'header.php';
+?>
 
 <div id="resultsDiv">
     <?php
-    echo "<h3>Recipes containing: " . $_REQUEST["ingred1"] . ",  " . $_REQUEST["ingred2"] . ",  " . $_REQUEST["ingred3"] ."</h3><br><br>";
+    if($_REQUEST) {
+        echo "<h3>Recipes containing: " . $_REQUEST["ingred1"] . ",  " . $_REQUEST["ingred2"] . ",  " . $_REQUEST["ingred3"] . "</h3><br><br>";
 
+        $sql = "SELECT * FROM lewischr_recipes.all_data_view
+    WHERE ingredient IN ('" . $_REQUEST["ingred1"] . "' , '" . $_REQUEST["ingred2"] . "', '" . $_REQUEST["ingred3"] . "') GROUP BY title";
+    } else {
+        $sql = "SELECT * FROM lewischr_recipes.all_data_view WHERE 1 group by ID";
+    }
+    if ($result = $mysqli->query($sql)) {
 
+        while ($row = $result->fetch_assoc()) {
+            echo '<div class="searchResult">
+            <img class="recipeImage" src="'.$row["imgURL"].'">
+            <div class="recipeInfo">
+            <span class="recipeName"><strong>' . $row['title'] . '</strong>
+            <a href="'.$row["url"].'" target="_blank" ><img class="outLink" src="Asset%204.png"> </a> 
+            <br></span>
+            <em>' . $row['description'] . '</em>
+            <br>';
+            //echo '<div class="tags">'.$row["ID"].'</div>';
+
+            $sqlMeals = "SELECT * FROM lewischr_recipes.recipe_meal_join WHERE mID=" . $row["ID"];
+            //echo $sqlDiets;
+            if ($mealTagResult = $mysqli->query($sqlMeals)) {
+                while ($mealTagRow = $mealTagResult->fetch_assoc()) {
+                    //echo '<div class="tags">'.$row["ID"].'</div>';
+                    echo '<div class="tags">'.$mealTagRow["meal"].'</div>';
+                    //var_dump($mysqli);
+                }
+            }
+            else {
+                var_dump($mysqli);
+            }
+            $sqlDiets = "SELECT * FROM lewischr_recipes.recipe_diet_join WHERE dID=" . $row["ID"];
+            //echo $sqlDiets;
+            if ($dietTagResult = $mysqli->query($sqlDiets)) {
+                while ($dietTagRow = $dietTagResult->fetch_assoc()) {
+                    //echo '<div class="tags">'.$row["ID"].'</div>';
+                    echo '<div class="tags">'.$dietTagRow["diet"].'</div>';
+                    //var_dump($mysqli);
+                }
+            }
+            else {
+                var_dump($mysqli);
+            }
+            echo '</div></div>';
+        }
+    }
     ?>
-
-    <div class="HRecipe"> Recipe </div><div class="HDescr"> Description </div ><div class="HLink"> Recipe Link </div> <br><hr>
-
-<?php
-$sql = "
-    SELECT *
-    FROM lewischr_recipes.recipe r
-    JOIN lewischr_recipes.recipe_ingredient ri ON r.ID = ri.recipe_ID
-JOIN lewischr_recipes.ingredients i ON i.ID = ri.ingredient_ID
-    WHERE i.ingredient IN ('" . $_REQUEST["ingred1"] . "' , '" . $_REQUEST["ingred2"] . "', '" . $_REQUEST["ingred3"] . "')";
-
-//echo $sqli;
-
-$results = $mysqli -> query($sql);
-
-if(!$results){
-    echo "<br>";
-    echo "SQL error: " . $mysqli -> error;
-}
-
-while($currentrow = $results -> fetch_assoc()){
-    echo "<div class='resultsRecipe'>" . $currentrow['recipename'] . "</div>" . "<div class='resultsDescr'>" . $currentrow['description'] . "</div>" . "<div class='resultsHyper'><a class='resultslink' href='". $currentrow['url']. "'>" . "link here" . "</a></div>".  "<br></nr><br>";
-}
-?>
 </div>
 
 </body>
