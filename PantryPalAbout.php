8ea52ba4962b4cad7c3d763fdfa884b4bcb55c13
Alyssa <aegoldbe@usc.edu>
2018-12-03 18:55:04 -0800

    New Static Pages

---
+++ PantryPalAbout.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <link rel="stylesheet" href="pantryStyle.css">
+</head>
+<body>
+    <div style="background-color:#badee2; width: 100%; padding:50px 50px 50px 0px">
+        <h1>About Us</h1><br>
+        <p style="text-align: center; margin: auto; width: 750px">PantryPal was created by a team of four college sophomores who just moved into their apartments and never know what to cook. Looking up recipes is always an option, but after that there is always the issue of going to get the ingredients needed. We were looking for a solution that helped us find recipes that used the ingredients we already have, so we didn't have to go through the hassle of buying them after. That's when PantryPal was born to help us cook, and many other college students!</p>
+        <br>
+    </div>
+    <div style=" padding: 30px 30px 50px 30px; float: center; text-align: center; margin: auto">
+        <h2>Team</h2><br>
+        <img style="width: 750px;" src="Pantry.png">
+    </div>
+    <div style="background-color:#69ccd7; width: 100%; padding:30px 50px 30px 0px; float: center; text-align: center; margin: auto"">
+        <h2>Contact</h2><br>
+    <form action="http://www.usc.edu/cgi-bin/feedback_form/aegoldbe@usc.edu/user_feedback/">
+        <div class="label"> Name :
+        <input type="text" style="width: 200px; padding: 10px; height: 30px; border-radius: 5px" name="fullname" class="medtext" placeholder="John Smith" required="1" title="First Last">
+        </div><br>
+        <div class="label"> Email :
+        <input type="text" style="width: 200px; padding: 10px; height: 30px; border-radius: 5px" name="email" class="medtext" placeholder="Johnsmith@mail.com" required="1">
+        </div><br>
+        <div class="label">
+            <textarea style="resize: none; width: 650px; padding: 10px; height: 80px; border-radius: 5px" name="email" class="medtext" placeholder="What are your thoughts?" required="1"></textarea>
+        </div><br>
+        <input type="submit" class="button" value="Submit" style=" width: 70px">
+        <br>
+    </form>
+        <br>
+    </div>
+    <div style=" padding: 30px 30px 50px 30px; float: center; text-align: center; margin: auto">
+        <br> &copy; PantryPal Inc.<br>
+    </div>
+</body>
+</html>
+
+<!--background-image: linear-gradient(#badee2, #69ccd7)
---
+++ PantryPalHowTo.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <link rel="stylesheet" href="pantryStyle.css">
+</head>
+<body>
+<div style="background-color:#badee2; width: 100%; padding:50px 50px 50px 0px">
+    <h1>How To Use PantryPal!</h1><br>
+    <p style="text-align: center; margin: auto; width: 750px">Welcome to the site! Navigating should be pretty easy, but here are some simple tips.</p>
+    <br>
+</div>
+<div style=" padding: 50px 30px 30px 30px">
+    <div style="width:850px; height: 400px; margin: auto">
+        <img style="width: 400px; float: left; border-radius: 20px; margin: 0px 50px 0px 0px" src="Ingredients.png">
+        <p style="line-height: 150%; padding-top: 150px">Start by choosing a few ingredients! Look in your pantry, fridge, cabinets, etc. to find what food you have to cook with. This can vary from main proteins like chicken, to vegetables like broccoli, seasonings like rosemary, and more!</p>
+    </div>
+</div>
+<div style=" padding: 0px 30px 0px 30px">
+    <div style="width:850px; height: 510px; margin: auto">
+        <img style="height: 500px; float: right; margin: 0px 0px 0px 50px" src="Chicken.png">
+        <p style="line-height: 150%; padding-top: 180px; text-align: right">Then pick a recipe that you like! All of them should match the ingredients that you searched and now you just have to choose what's best. For example, here is a Sesame Chicken "card" that you can take a look at. Click the arrow on the right to read more, and get the actual recipe.</p>
+    </div>
+</div>
+<div style=" padding: 0px 30px 30px 30px">
+    <div style="width:850px; height: 600px; margin: auto">
+        <img style="width: 400px; float: left; border-radius: 20px; box-shadow:3px 3px 10px 0px #e8e8e8; margin: 0px 50px 0px 0px" src="Recipe.png">
+        <p style="line-height: 150%; padding-top: 190px">After you click on the arrow, the recipe on it's original site will pop up with the exact ingredients, directions, and sometimes reviews. You can read more about the recipe and start cooking yourself! Some of the recipes will require a few extra ingredients on top of what you entered, but hopefully this will help give you a good starting point.</p>
+    </div>
+</div>
+<div style="background-color:#69ccd7; width: 100%; padding:30px 50px 10px 0px; float: center; text-align: center; margin: auto"">
+<h2>We hope this helps! Happy cooking :)</h2><br>
+</div>
+<div style=" padding: 30px 30px 50px 30px; float: center; text-align: center; margin: auto">
+    <br> &copy; PantryPal Inc.<br>
+</div>
+</body>
+</html>
+
+<!--background-image: linear-gradient(#badee2, #69ccd7)
